e7b39219cb619aec76785e010ccb45362a928193
Logan Negley <logannegley@gmail.com>
2019-11-08 22:14:26 -0600

    finished tabs

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,36 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+const promise = axios.get("https://lambda-times-backend.herokuapp.com/topics");
+
+promise.then(response => {
+  console.log(response);
+  const topics = response.data.topics;
+
+  //   for (i = 0; i < topics.length; i++) {
+  //     const greatTab = topics[i];
+  //     const entryPoint = document.querySelector(".topics");
+  //     entryPoint.append(newTab(greatTab));
+  //   }
+
+  topics.forEach(item => {
+    console.log(item);
+    const entryPoint = document.querySelector(".topics");
+    const tab = newTab(item);
+    entryPoint.append(tab);
+  });
+});
+
+function newTab(obj) {
+  //  Create Element
+  const tab = document.createElement("div");
+
+  //Add Class
+  tab.classList.add("tab");
+
+  //Add Content
+  tab.textContent = obj;
+
+  return tab;
+}
